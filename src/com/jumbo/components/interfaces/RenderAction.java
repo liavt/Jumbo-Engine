cf4156c9c2f35eb5674a930be89a5cad21f7dae3
liavt <liavt@yahoo.com>
2015-10-20 16:08:05 -0500

    Minor bug fix

--- src/com/jumbo/components/interfaces/RenderAction.java
+++ src/com/jumbo/components/interfaces/RenderAction.java
@@ -1,5 +1,5 @@
 package com.jumbo.components.interfaces;
 
 public interface RenderAction extends java.io.Serializable{
-  public void action(JumboEntity e);
+  public void action(JumboGraphicsObject e);
 }
